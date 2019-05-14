414508e82ae4407aed580afe9108264d48250920
Ayslu <aislu-bgu@yandex.ru>
2019-05-14 10:17:30 +0300

    Изменения по работе с генерацией текста

--- src/part01/lesson06/task02/Main.java
+++ src/part01/lesson06/task02/Main.java
@@ -1,25 +1,65 @@
 package part01.lesson06.task02;
 
+import java.io.DataOutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Random;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        Random random = new Random();
-        String s = "";
-        Words words = new Words();
+        //Words words = new Words();
+//        System.out.println("это случайное слово " + Words.randomWord());
+//        System.out.println("это случайная фраза " + Words.randomPhrase());
+//        System.out.println("Это случайный абзац " + Words.createIndent() );
+//        System.out.println("Это случайный текст " + Words.createText());
 
-        //words.randomWord(random.nextInt(15));
-        System.out.println("это случайное слово " + words.randomWord(random.nextInt(15)));
+        String[] library = Words.createWordsArray();
 
-        HashSet<String> library = words.createWordsArray(20);  // надо потом задать рандомный объем
-        System.out.println("это словарь");
-        library.forEach(System.out::println);
+        getFiles("/Users/timur/projects/UI_STC/lesson06-2/", 5, 10, library, 10);
+        // /Users/timur/projects/UI_STC/
 
-        System.out.println("это случайная фраза " + words.randomPhrase(random.nextInt(15)));
+    }
+
+    static void getFiles(String path, int n, int size, String[] words, int probability) throws FileNotFoundException {
+
+       Random random = new Random();
+
+       StringBuilder text = Words.createText();
+       String str = String.valueOf(text);
+       String[] wordsFromText = str.split(" ");
+
+       int changingWords = wordsFromText.length % probability;
+
+       for (int i = 0; i < changingWords; i++) {
+           int randomWordIndex = random.nextInt(wordsFromText.length - 1);
+           wordsFromText[randomWordIndex] = words[random.nextInt(words.length - 1)];
+       }
+
+        String newText = String.join(" ", wordsFromText);
+
+        System.out.println(newText);
+
+        String[] names = new String[n];
+        for (int i = 0; i < n; i++) {
+            names[i] = "tmp" + i;
+            File newFile = new File(path, names[i]);
+        try {
+            newFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+
+//
+
+
+//        for (String str1: file.list()) {
+//            System.out.println(str1);
+//        }
 
     }
 }
--- src/part01/lesson06/task02/Words.java
+++ src/part01/lesson06/task02/Words.java
@@ -7,10 +7,18 @@ import java.util.Random;
 public class Words {
 
     private static char[] symbols = {'(', '.', '|', '!', '|', '?', ')', '+', '"', '"'};
+    private static final int MAX_LENGHT = 15;
+    private static final int MAX_PHRASES_COUNT = 20;
+    private static final int MAX_ARRAY = 1000;
 
-    public static String randomWord(int length) {
+    /**
+     * Составляет случайное слово
+     * @return
+     */
+    public static String randomWord() {
         Random random = new Random();
-        StringBuilder word = new StringBuilder(length);
+        StringBuilder word = new StringBuilder();
+        int length = 1 + (int) ( Math.random() * MAX_LENGHT );
         for (int i = 0; i < length; i++) {
             word.append((char)('a' + random.nextInt(26)));
         }
@@ -18,30 +26,81 @@ public class Words {
         return word.toString();
     }
 
-    public static String randomPhrase(int length) {
+    /**
+     * Составляет предложение из случайных слов
+     * @return
+     */
+    public static String randomPhrase() {
         Random random = new Random();
+        int length = 1 + (int) ( Math.random() * MAX_LENGHT );
         StringBuilder phrase = new StringBuilder(length);
         for (int i = 0; i < length ; i++) {
-            phrase.append(randomWord(random.nextInt(15))).append(" ");
+            phrase.append(randomWord());
+            int comma = random.nextInt(10);
+            if (comma == 5 & i != length-1) {
+                phrase.append(",");
+            }
+            phrase.append(" ");
         }
 
         char first = Character.toUpperCase(phrase.charAt(0));
         phrase.setCharAt(0, first );
-
-        int endOfPhrase = phrase.length()-1;
-        phrase.setCharAt(endOfPhrase, symbols[random.nextInt(10)]);
+        phrase.setCharAt(phrase.length()-1, symbols[random.nextInt(10)]);
 
         return phrase.toString();
     }
 
-    public HashSet<String> createWordsArray(int volume) {
+    
+    /**
+     * Создает абзац из случайных слов
+     * @return
+     */
+    public static String createIndent() {
+        StringBuilder indent = new StringBuilder();
+        int length = 1 + (int) ( Math.random() * MAX_PHRASES_COUNT );
+        indent.append("\t");
+        for (int i = 0; i < length; i++) {
+            indent.append(randomPhrase());
+            indent.append(" ");
+        }
+        indent.append("\n");
+
+        return indent.toString();
+    }
+
+    
+    /**
+     * Создает текст из случайных абзацев
+     * @return
+     */
+    public static StringBuilder createText() {
         Random random = new Random();
-        HashSet<String> wordsLibrary = new HashSet<>();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < random.nextInt(10); i++) {
+            text.append(createIndent());
+        }
+
+        return text;
+    }
+
+    
+
+     /**
+     * Создает массив уникальных слов
+     * @return
+     */
+    public static String[] createWordsArray() {
+        int volume = 1 + (int) ( Math.random() * MAX_ARRAY );
+        String[] wordsLibrary = new String[volume];
 
         for (int i = 0; i < volume; i++) {
-            wordsLibrary.add(randomWord(random.nextInt(15)));
+            wordsLibrary[i] = randomWord();
         }
 
         return wordsLibrary;
     }
+
+
+
+
 }
