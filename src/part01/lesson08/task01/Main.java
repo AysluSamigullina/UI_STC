c9b868f2b30eb6d5a8b37b809c904467defdb4fc
Ayslu <aislu-bgu@yandex.ru>
2019-05-18 00:02:04 +0300

    ДЗ №7 про сериализацию и десериализацию

--- src/part01/lesson08/task01/Main.java
+++ src/part01/lesson08/task01/Main.java
@@ -2,40 +2,46 @@ package part01.lesson08.task01;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Основной класс
+ */
 
 public class Main {
 
-    public static void main(String[] args) {
-        Person person1 = new Person("Kate", 20, 50, false);
-        Person person2 = new Person("Misha", 39, 70, true);
-        Person person3 = new Person("Anna", 32, 45, false);
-        Person person4 = new Person("Ivan", 45, 78, true);
+    /**
+     * Основной метод
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Kate", 20, 50, false));
+        personList.add(new Person("Misha", 39, 70, true));
+        personList.add(new Person("Anna", 32, 45, false));
+        personList.add(new Person("Ivan", 45, 78, true));
 
         String fileName = "lesson08";
+        Serial serial = new Serial(fileName);
 
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
-            objectOutputStream.writeObject(person1);
-            objectOutputStream.writeObject(person2);
-            objectOutputStream.writeObject(person3);
-            objectOutputStream.writeObject(person4);
-
-            objectOutputStream.writeObject(new Person("Tom", 34, 67, true));
-            person1.setName("Rita");
-            objectOutputStream.reset();
-            objectOutputStream.writeObject(person1);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        /**
+         * Сериализация в файл
+         */
+        personList.forEach(person ->
+                serial.serialize(person, fileName)
+        );
 
         ArrayList<Person> lines = new ArrayList<>();
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(fileName))) {
-            for (int i = 0; i < 5; i++) {
-                lines.add((Person) objectInputStream.readObject());
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+
+        /**
+         * Десериализация из файла
+         */
+        for (int i = 0; i < personList.size(); i++) {              // чем заменить окончание цикла?
+            lines.add((Person) serial.deSerialize(fileName));
         }
+
         lines.forEach(System.out::println);
 
     }
--- src/part01/lesson08/task01/Person.java
+++ src/part01/lesson08/task01/Person.java
@@ -2,22 +2,31 @@ package part01.lesson08.task01;
 
 import java.io.Serializable;
 
+/**
+ * Человек
+ */
 public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String name;
     private int age;
     private int weight;
     private boolean sex;
 
+    /**
+     * Конструктор
+     *
+     * @param name
+     * @param age
+     * @param weight
+     * @param sex
+     */
     public Person(String name, int age, int weight, boolean sex) {
         this.name = name;
         this.age = age;
         this.weight = weight;
         this.sex = sex;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
